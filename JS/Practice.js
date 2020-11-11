c537b6bc3a9065eb5916514bf666672da34d82fe
Walter Smith <walter.c.smith01@gmail.com>
2020-11-11 11:56:03 -0600

    Another go at commiting

---
+++ JS/Practice.js
@@ -0,0 +1,5 @@
+var askHowAreYou = prompt("How are you doing?");
+alert("I'm glad you're doing " + askHowAreYou);
+var pickAColor = prompt("Choose between the color red or blue: ");
+alert("You chose the color: " + pickAColor + " which is a great choice.");
+console.log("You chose the color: " + pickAColor + " which is a great choice.");
