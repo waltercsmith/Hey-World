60415c1ba2eb4d661041c04aa2ed687176781628
Walter Smith <walter.c.smith01@gmail.com>
2021-01-06 16:08:07 -0600

    Hey-World:nav-bar.js:Refactored code and added comments for clarity

--- JS/nav-bar.js
+++ JS/nav-bar.js
@@ -12,3 +12,11 @@ miles = 1230;
 kilometers = miles * 1.60934;
 console.log(kilometers);
 
+//created a variable with a string value
+var song = "Don't stop me now";
+//created a variable called included to keep track whether a song is already included or not but the song is already included so will use the value of true for the variable included
+var included = true;
+//writing a print statment to show the playlist new entry
+console.log();
+//displayed the string new entry and the variable song in the console to let the user know that their song is added
+console.log("New Entry: " + song);
