b61dfb11a4f388dba2859db253356a894525617b
Walter Smith <walter.c.smith01@gmail.com>
2021-01-06 15:55:20 -0600

    Hey-World:nav-bar.js:created a simple miles to kilometers converter

--- JS/nav-bar.js
+++ JS/nav-bar.js
@@ -3,10 +3,10 @@
 var miles;
 // created a kilometers variable & gave it the value of miles
 miles = 500;
-var kilometers = miles;
-kilometers = miles * 1.60934;
+var kilometers = miles * 1.60934;
 //using console.log() to display the value of kilometers
 console.log(kilometers);
 //the user can change the value of miles to see the value of kilometers
-
+miles = 1230;
+console.log(kilometers);
 
