74705bbed1a3249cff3bf3ba62eca79f57f9e29a
Walter Smith <walter.c.smith01@gmail.com>
2021-01-06 16:01:03 -0600

    Hey-World:nav-bar.js:Refactored code and added comments for clarity

--- JS/nav-bar.js
+++ JS/nav-bar.js
@@ -1,12 +1,14 @@
 "use strict"
 //converting miles to KM
-var miles;
 // created a kilometers variable & gave it the value of miles
-miles = 500;
+var miles = 500;
 var kilometers = miles * 1.60934;
 //using console.log() to display the value of kilometers
 console.log(kilometers);
 //the user can change the value of miles to see the value of kilometers
+// also in order to see a different value, the coder must rewrite the
+// kilometers = miles * 1.60934
 miles = 1230;
+kilometers = miles * 1.60934;
 console.log(kilometers);
 
