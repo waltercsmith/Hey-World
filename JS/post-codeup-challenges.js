d1f21063f82ccbb21d0ae1b20de25038b845a003
Walter Smith <walter.c.smith01@gmail.com>
2021-05-21 13:32:09 -0500

    Feat:Created a FizzBuzz Challenge were if the number from 1 to 100 is multiple of 3 print Fizz, if a multiple of 5 print Buzz, if a multiple of both 3 and 5 print FizzBuzz

--- JS/post-codeup-challenges.js
+++ JS/post-codeup-challenges.js
@@ -1,5 +1,22 @@
 "use strict";
 
-for(var i = 0; i <= 101; i++){
-    console.log(i)
+//  Created a for loop that will print into the console 1 to 100
+// for(var num = 1; i < 101; num++){
+//     console.log(num)
+// }
+
+
+//  Created a loop with if/else condition: FizzBuzz Challenge
+for(var i = 1; i < 101; i++){
+    if (i % 3 !== 0 && i % 5 !== 0){
+        console.log(i);
+    }   else if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }
+        else if( i % 3 === 0){
+        console.log("Fizz");
+    }   else if(i % 5 === 0){
+        console.log("Buzz");
+    }
+
 }
